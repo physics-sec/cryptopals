1d4a20d185f6a903ba4d5566e3eef8ccf13010ef
physics-sp <physics-sp@protonmail.com>
2020-02-14 11:31:49 -0300

    discard invalid plaintexts in single_byte_xor_cypher.py

--- Set_1/single_byte_xor_cypher.py
+++ Set_1/single_byte_xor_cypher.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import string
+
 ct = bytes.fromhex('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736').decode('utf-8')
 
-frec = {}
+def find_single_byte_xor(ct):
+	frec = {}
 
-for b in ct:
-	if str(b) not in frec:
-		frec[str(b)] = 1
-	else:
-		frec[str(b)] += 1
+	for b in ct:
+		if str(b) not in frec:
+			frec[str(b)] = 1
+		else:
+			frec[str(b)] += 1
 
-frec = sorted(frec, key=lambda elem: -frec[elem[0]])
+	frec = sorted(frec, key=lambda elem: -frec[elem[0]])
 
-for b in frec:
-	# space is likely the most common char in plain text
-	key = ord(' ') ^ ord(b)
-	r = b''
-	for i in range(len(ct)):
-		r += bytes([ ord(ct[i]) ^ key ])
-	r = str(r,'ascii')
-	print(r)
+	resp = []
+	for b in frec:
+		# space is likely the most common char in plain text
+		key = ord(' ') ^ ord(b)
+		r = b''
+		for i in range(len(ct)):
+			r += bytes([ ord(ct[i]) ^ key ])
+		r = str(r,'ascii')
+		valid = True
+		for b in r:
+			if b not in string.printable:
+				valid = False
+				break
+		if valid:
+			resp.append(r)
+	return resp
+
+print(find_single_byte_xor(ct)[0])
