e45e2048d9224ffd5e3d267ce5a0f6e2971e520f
physics-sp <physics-sp@protonmail.com>
2020-02-14 11:18:12 -0300

    start changing from python2 to python3

--- Set_1/fixed_xor.py
+++ Set_1/fixed_xor.py
@@ -1,18 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-x1 = '1c0111001f010100061a024b53535009181c'.decode('hex')
+import codecs
 
-x2 = '686974207468652062756c6c277320657965'.decode('hex')
+def xor(x1, x2):
+	assert len(x1) == len(x2)
+	r = b''
+	for i in range(len(x1)):
+		r += bytes([ ord(x1[i]) ^ ord(x2[i]) ])
+	return codecs.encode(r, 'hex')
 
-assert len(x1) == len(x2)
+x1 = bytes.fromhex('1c0111001f010100061a024b53535009181c').decode('utf-8')
+x2 = bytes.fromhex('686974207468652062756c6c277320657965').decode('utf-8')
 
-r = ''
-for i in xrange(len(x1)):
-	r += chr( ord(x1[i]) ^ ord(x2[i]) )
+r = xor(x1, x2)
 
-r = r.encode('hex')
-
-print r
-
-
+print(r)
--- Set_1/hex_to_b64.py
+++ Set_1/hex_to_b64.py
@@ -5,9 +5,7 @@ import base64
 
 string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 
-s = string.decode('hex')
-
-print type(s)
+s = bytes.fromhex(string)
 
 b64 = base64.b64encode(s)
 
