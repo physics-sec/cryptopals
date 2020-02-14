5b326a6699332639fb55b5b8675fc77d0df6eadc
physics-sp <physics-sp@protonmail.com>
2020-02-14 11:37:14 -0300

    just handle bytes in single_byte_xor_cypher.py

--- Set_1/single_byte_xor_cypher.py
+++ Set_1/single_byte_xor_cypher.py
@@ -3,26 +3,26 @@
 
 import string
 
-ct = bytes.fromhex('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736').decode('utf-8')
+ct = bytes.fromhex('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
 
 def find_single_byte_xor(ct):
 	frec = {}
 
 	for b in ct:
-		if str(b) not in frec:
-			frec[str(b)] = 1
+		if b not in frec:
+			frec[b] = 1
 		else:
-			frec[str(b)] += 1
+			frec[b] += 1
 
-	frec = sorted(frec, key=lambda elem: -frec[elem[0]])
+	frec = sorted(frec, key=lambda elem: -frec[elem])
 
 	resp = []
 	for b in frec:
 		# space is likely the most common char in plain text
-		key = ord(' ') ^ ord(b)
+		key = ord(' ') ^ b
 		r = b''
 		for i in range(len(ct)):
-			r += bytes([ ord(ct[i]) ^ key ])
+			r += bytes([ ct[i] ^ key ])
 		r = str(r,'ascii')
 		valid = True
 		for b in r:
