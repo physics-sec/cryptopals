2a4f32b6adb9bce8d84ad6d2b09c1865cb316623
physics-sp <physics-sp@protonmail.com>
2020-03-24 12:18:06 -0300

    done CBC_bitflipping_attacks.py

--- Set_2/CBC_bitflipping_attacks.py
+++ Set_2/CBC_bitflipping_attacks.py
@@ -3,7 +3,6 @@
 
 import os
 import random
-import string
 from Crypto.Cipher import AES
 
 def rand_bytes(len):
@@ -87,33 +86,40 @@ def enc(user_input):
 
 def dec(ct):
 	pt = decrypt_AES_CBC(ct)
-	if pt != b'':
-		pass#print(pt)
-	return b";admin=true;" in pt
-
-def randomString(stringLength):
-	letters = string.ascii_lowercase
-	return ''.join(random.choice(letters) for i in range(stringLength))
+	#if pt != b'':
+	#	print(pt)
+	return pt
 
 def bit_flip():
-	while True:
-		rs = randomString(40).encode('utf-8')
-		ct_original = enc(rs)
-		for i in range(len(ct_original)):
-			for j in range(8):
-				ct  = ct_original[:i]
-				ct += bytes( [ct_original[i] ^ (1 << j)] )
-				ct += ct_original[i+1:]
-				if dec(ct):
+	# comment1=cooking %20MCs;userdata= ?admin?true;comm ent2=%20like%20a %20pound%20of%20 bacon
+
+	payload  = bytes( [ ord(';') ^ (1 << random.randint(0, 7))] )
+	payload += b'admin'
+	payload += bytes( [ ord('=') ^ (1 << random.randint(0, 7))] )
+	payload += b'true'
+
+	pos_1 = 6 + 16
+	pos_2 = 0 + 16
+
+	ct_original = enc(payload)
+	for i in range(8):
+		ct  = ct_original[:pos_1]
+		ct += bytes( [ct_original[pos_1] ^ (1 << i)] )
+		ct += ct_original[pos_1+1:]
+		pt = dec(ct)
+		if b"admin=true" in pt:
+			ct_original = ct
+			for i in range(8):
+				ct  = ct_original[:pos_2]
+				ct += bytes( [ct_original[pos_2] ^ (1 << i)] )
+				ct += ct_original[pos_2+1:]
+				pt = dec(ct)
+				if b";admin=true;" in pt:
 					print('exito!')
-					print('i:{:d}'.format(i))
-					print('j:{:d}'.format(j))
 					print('ct:')
 					print(ct)
-					print('ct_original:')
-					print(ct_original)
-					print('pt:')
-					print(decrypt_AES_CBC(ct))
+					print('pt')
+					print(pt)
 					return
 
 if __name__ == '__main__':
