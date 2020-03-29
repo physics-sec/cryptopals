f631128630184844bb35b4b263c3ea4cc8a3312d
physics-sp <physics-sp@protonmail.com>
2020-03-29 16:13:13 -0300

    improve Implement_RSA.py

--- Set_5/Implement_RSA.py
+++ Set_5/Implement_RSA.py
@@ -3,38 +3,38 @@
 
 import subprocess
 
-def egcd(a, b):
-	if a == 0:
-		return (b, 0, 1)
-	else:
-		g, y, x = egcd(b % a, a)
-		return (g, x - (b // a) * y, y)
-
-def invmod(a, m):
-	g, x, y = egcd(a, m)
-	if g != 1:
-		raise Exception('modular inverse does not exist')
-	else:
-		return x % m
-
-def gen_prime():
-	prime_size =  512
-	process = subprocess.Popen(['openssl', 'prime', '-generate', '-bits', str(prime_size), '-hex'], stdout=subprocess.PIPE)
-	prime = process.communicate()[0][:-1]
-	prime = int(prime.decode('utf-8'), 16	)
-	return prime
-
 class RSA():
 
+	def egcd(self, a, b):
+		if a == 0:
+			return (b, 0, 1)
+		else:
+			g, y, x = self.egcd(b % a, a)
+			return (g, x - (b // a) * y, y)
+
+	def invmod(self, a, m):
+		g, x, y = self.egcd(a, m)
+		if g != 1:
+			raise Exception('modular inverse does not exist')
+		else:
+			return x % m
+
+	def gen_prime(self):
+		prime_size =  512
+		process = subprocess.Popen(['openssl', 'prime', '-generate', '-bits', str(prime_size), '-hex'], stdout=subprocess.PIPE)
+		prime = process.communicate()[0][:-1]
+		prime = int(prime.decode('utf-8'), 16	)
+		return prime
+
 	def gen_keypair(self):
-		p = gen_prime()
-		q = gen_prime()
+		p = self.gen_prime()
+		q = self.gen_prime()
 		if p == q:
 			return self.gen_keypair()
 		self.n = p * q
 		et = (p - 1) * (q - 1)
 		self.e = 3
-		self.d = invmod(self.e, et)
+		self.d = self.invmod(self.e, et)
 
 	def get_public_key(self):
 		return [self.e, self.n]
@@ -48,7 +48,7 @@ class RSA():
 			n = self.n
 		plaintext  = plaintext.hex()
 		plaintext  = int(plaintext, 16)
-		if plaintext > self.n:
+		if plaintext > n:
 			raise Exception('plaintext is longer than n')
 		ciphertext = pow(plaintext, e, n)
 		ciphertext = "%x" % ciphertext
@@ -74,4 +74,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
