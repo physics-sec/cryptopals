3f87aff905f2309749c99d8ec34efdbaadbcf924
physics-sp <physics-sp@protonmail.com>
2020-04-04 17:50:27 -0300

    small refactor

--- Set_5/key-fixing_attack_on_Diffie-Hellman_with_parameter_injection.py
+++ Set_5/key-fixing_attack_on_Diffie-Hellman_with_parameter_injection.py
@@ -153,11 +153,9 @@ def pad(s, pad_len=16):
 	return s + pad
 
 def xor(x1, x2):
-	assert len(x1) == len(x2)
-	r = b''
-	for i in range(len(x1)):
-		r += bytes([ x1[i] ^ x2[i] ])
-	return r
+    assert len(x1) == len(x2)
+    b_list = list(map(lambda x,y: x^y, x1, x2))
+    return bytes( b_list )
 
 def encrypt_AES_ECB(plaintext, key):
 	assert len(key) == 128/8
